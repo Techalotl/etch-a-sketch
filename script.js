6f008318d0c21ac9e9ab0578450ef0cb1fa5baad
Adriana Coll <169408160+Techalotl@users.noreply.github.com>
2024-08-27 19:45:50 -0600

    Fix grid and add function for a button
    The initial way of setting the grid was changed.
    Now button "change grid" works correctly.

--- script.js
+++ script.js
@@ -2,20 +2,31 @@ const colorButton = document.querySelector(".color-button");
 const gridButton = document.querySelector(".grid-button");
 const eraseButton = document.querySelector(".erase-button");
 const gridContainer = document.querySelector(".grid-container");
-let squaresPerSide = 3;
-let totalSquares = squaresPerSide * squaresPerSide
-const gridSize = 420;
-let squareSize = (gridSize/squaresPerSide)+"px";
-console.log(squareSize);
-console.log(totalSquares);
 const div = document.createElement("div");
+let squaresPerSide = 16;
+const gridSize = 420;
+let squareSize = (gridSize/squaresPerSide)+"px"
+console.log(squareSize);
+div.style.height = squareSize;
+div.style.width = squareSize;
+div.style.border = " 1px solid white";
+div.setAttribute("class", "square");
 
+for(let i = 0; i < (squaresPerSide*squaresPerSide); i++) {
+    gridContainer.appendChild(div.cloneNode(true));
+}
 
-function createGrid (numberOfSquares) {
-    for(let i = 0; i < numberOfSquares; i++) {
-        div.style.height = squareSize;
-        div.style.width = squareSize;
-        div.style.border = " 1px solid white";
+function removeFirstGrid(element) {
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+}
+
+function changeGrid(squaresPerSide) {
+    squareSize = (gridSize/squaresPerSide)+"px"
+    div.style.height = squareSize;
+    div.style.width = squareSize;
+    for(let i = 0; i < (squaresPerSide*squaresPerSide); i++) {
         gridContainer.appendChild(div.cloneNode(true));
     }
 }
@@ -25,10 +36,9 @@ gridButton.addEventListener("click", () => {
     if (squaresPerSide === null || isNaN(squaresPerSide) === true ||
         squaresPerSide < 1 || squaresPerSide > 100) {
         alert("That's not a valid number!");
-        squaresPerSide = 16;
+        return;
     }
+    removeFirstGrid(gridContainer);
+    changeGrid(squaresPerSide);
     console.log(squaresPerSide);
-    createGrid(squaresPerSide);
 })
-
-document.addEventListener("load", createGrid(totalSquares));
